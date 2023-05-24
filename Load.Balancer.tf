6fc0c43d80dade86ee4fe797ad0e74399dfabdd7
kits26kat24 <k.tijani@hotmail.com>
2023-05-24 11:20:10 +0100

    test 3b group completed work

--- Load.Balancer.tf
+++ Load.Balancer.tf
@@ -1,38 +1,40 @@
-resource "aws_lb" "test" {
-  name               = "test-lb-tf"
+
+resource "aws_lb" "test3b_lb" {
+  name               = "Test3B-LB"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.testcase_sg.id]
-  subnets            = [aws_subnet.ubuntu_main.id, aws_subnet.Amazon_Linux_main.id]
+  subnets            = [aws_subnet.Amazon_Linux_main.id, aws_subnet.ubuntu_main.id]
 
   enable_deletion_protection = false
 
-  #   access_logs {
-  #     bucket  = aws_s3_bucket.lb_logs.id
-  #     prefix  = "test-lb"
-  #     enabled = true
-  #   }
+  # access_logs {
+  #   bucket  = aws_s3_bucket.project03.bucket
+  #   prefix  = "test-lb"
+  #   enabled = true
+  # }
 
   tags = {
-    Environment = "production"
+    Environment = "dev"
   }
 }
-resource "aws_lb_listener" "test_front_end" {
-  load_balancer_arn = aws_lb.test.arn
+
+resource "aws_lb_listener" "test3b_front_end" {
+  load_balancer_arn = aws_lb.test3b_lb.arn
   port              = "80"
-  protocol          = "HTTPS"
-  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+  protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.test_end.arn
+    target_group_arn = aws_lb_target_group.test3b.arn
   }
 }
 
-resource "aws_lb_target_group" "test_end" {
-  name     = "tf-example-lb-tg"
+resource "aws_lb_target_group" "test3b" {
+  name     = "Test3B-lb-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.testcase_main.id
 }
+
 
